044ee94c6fe8371a48e4ef66517777b5affeada2
Forbes Gray <fnbg75@gmail.com>
2016-05-06 16:03:08 +0100

    remove url from input on recipe render

--- public/js/controllers/home-controller.js
+++ public/js/controllers/home-controller.js
@@ -2,8 +2,6 @@
 	'use strict';
 	angular.module('app')
 	  .controller('homeController', ['$scope', '$http', function ($scope, $http) {
-	  	$scope.name = "Forbes";
-
 	  	$scope.getRecipe = function () {
 	  		var url = '/api/scrape?url=' + $scope.recipe.url;
 	  		$http.get(url).then(function (response, error) {
@@ -11,6 +9,7 @@
 	  				return $scope.error = error;
 	  			}
 	  			$scope.json = response.data;
+					$scope.recipe.url = "";
 	  		});
 	  	};
 	  }]);
