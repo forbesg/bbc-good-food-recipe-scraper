97e088d9932a87c5c2df9503f5def2e35b2df031
Forbes Gray <fnbg75@gmail.com>
2021-08-03 22:10:57 +0100

    Update element queries for new BBC template

--- routes/api/scrape.js
+++ routes/api/scrape.js
@@ -1,70 +1,68 @@
-var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio');
+var express = require("express");
+var request = require("request");
+var cheerio = require("cheerio");
 var router = express();
 
-router.all('/', function(req, res, next) {
+router.all("/", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
- });
+});
 
-router.get('/', function (req, res) {
-	var url = req.query.url,
-			recipe = {
-				title: null,
-				cuisine: null,
-				ingredients: [],
-				method:[],
-				time: {
-					preparation: null,
-					cooking: null
-				},
-				serves: null,
-				rating: {
-					average: null,
-					count: null,
-					total: null
-				},
-				self_url: url
-			};
+router.get("/", function (req, res) {
+  var url = req.query.url,
+    recipe = {
+      title: null,
+      cuisine: null,
+      ingredients: [],
+      method: [],
+      time: {
+        preparation: null,
+        cooking: null,
+      },
+      serves: null,
+      rating: {
+        average: null,
+        count: null,
+        total: null,
+      },
+      self_url: url,
+    };
 
-	request(url, function (err, response, html) {
-		if (!err) {
-			var $ = cheerio.load(html);
-			recipe.title = $('h1.masthead__title.heading-1').text();
-			if (!recipe.title || recipe.title.length < 1) {
-				// res.sendStatus(404, 'Page not found');
-				return res.send({ error: "Not a valid BBC Good Food URL" });
-			} else {
-				$('.recipe-template__ingredients ul li.list-item').each(function (index, item){
-					// Trim string up to line break where ingredient anchor description starts
-					var lineBreak = $(this).text().indexOf('\n');
-					if (lineBreak > 0) {
-						recipe.ingredients.push($(this).text().substring(0, lineBreak));
-					} else {
-						recipe.ingredients.push($(this).text());
-					}
+  request(url, function (err, response, html) {
+    if (!err) {
+      var $ = cheerio.load(html);
+      recipe.title = $("h1.post-header__title.heading-1").text();
+      if (!recipe.title || recipe.title.length < 1) {
+        // res.sendStatus(404, 'Page not found');
+        return res.send({ error: "Not a valid BBC Good Food URL" });
+      } else {
+        $(".recipe__ingredients ul li.list-item").each(function (index, item) {
+          // Trim string up to line break where ingredient anchor description starts
+          var lineBreak = $(this).text().indexOf("\n");
+          if (lineBreak > 0) {
+            recipe.ingredients.push($(this).text().substring(0, lineBreak));
+          } else {
+            recipe.ingredients.push($(this).text());
+          }
+        });
+        $(".recipe__method-steps ul .list-item p").each(function (index, item) {
+          recipe.method.push($(this).text());
+        });
+        recipe.time = {
+          prep: $(".cook-and-prep-time .list").find("time").first().text(),
+          cook: $(".cook-and-prep-time .list").find("time").last().text(),
+        };
+        recipe.serves = $(".post-header__servings").children().last().text();
+        recipe.image = $(".post-header__image-container img").attr("src");
 
-				});
-				$('.recipe-template__method-steps ul .list-item p').each(function (index, item) {
-					recipe.method.push($(this).text());
-				});
-				recipe.time = {
-					prep: $('.cook-and-prep-time .list').find('time').first().text(),
-					cook: $('.cook-and-prep-time .list').find('time').last().text()
-				}
-				recipe.serves = $('.masthead__servings').children().last().text();
-				recipe.image = $('.masthead__image img').attr('src');
-
-				res.send(recipe);
-			}
-
-		} else {
-			console.log(err);
-			res.send({ error: 'Invalid URI'});
-		}
-	})
+        res.send(recipe);
+      }
+    } else {
+      console.log(err);
+      res.send({ error: "Invalid URI" });
+    }
+  });
 });
 
 module.exports = router;
